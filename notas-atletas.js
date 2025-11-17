70dfce9665228a5ea011cf03e90405ae577b37a7
Thais Campos <168150247+snakegirlcode@users.noreply.github.com>
2025-11-17 13:46:42 -0300

    Update notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -17,18 +17,13 @@ let objAtletas = [
  }
 ];
 
-let media = []
-
 for (let i = 0; i < objAtletas.length; i++) {
   let novasNotas = objAtletas[i].notas.toSorted((a,b) => a - b).slice(1,4)
   let soma = novasNotas.reduce((x, y) => x + y)
-  media.push(soma/novasNotas.length)
-}
-
-for (let i = 0; i < objAtletas.length; i++) {
+  let media = soma/novasNotas.length
   objAtletas[i].notas.sort();
-  console.log('Nome:', objAtletas[i].nome,)
+  console.log('Nome:', objAtletas[i].nome)
   console.log('Notas Obtidas:',objAtletas[i].notas.join(", "))
-  console.log('Média Válida:', media[i])
+  console.log('Média Válida:', media)
   console.log("\n")
 }
